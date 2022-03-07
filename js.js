755b7f6f39a275c98ef8262c6d8a945f918d4d28
Piyush Mohapatra <32978741+piyush-mk@users.noreply.github.com>
2022-03-07 21:19:22 +0530

    Debug

--- js.js
+++ js.js
@@ -1,7 +1,17 @@
 const options = {
-  autoMatchOsTheme: false
+  bottom: '64px', // default: '32px'
+  right: 'unset', // default: '32px'
+  left: '32px', // default: 'unset'
+  time: '0.5s', // default: '0.3s'
+  mixColor: '#fff', // default: '#fff'
+  backgroundColor: '#fff',  // default: '#fff'
+  buttonColorDark: '#100f2c',  // default: '#100f2c'
+  buttonColorLight: '#fff', // default: '#fff'
+  saveInCookies: false, // default: true,
+  label: '☀️', // default: ''
+  autoMatchOsTheme: false // default: true
 }
 
 const darkmode = new Darkmode(options);
 darkmode.showWidget();
-darkmode.toggle();
+darkmode.toggle();                       
